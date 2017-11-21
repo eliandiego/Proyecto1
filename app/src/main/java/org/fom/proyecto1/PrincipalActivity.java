13db4a89a94342b826a7f760b0aba874e534e558
soniaporter <sonia_porter@hotmail.com>
2017-11-21 17:13:44 +0100

    Sonia: "Seccionando/animaciones"

--- app/src/main/java/org/fom/proyecto1/PrincipalActivity.java
+++ app/src/main/java/org/fom/proyecto1/PrincipalActivity.java
@@ -1,7 +1,12 @@
 package org.fom.proyecto1;
 //PANTALLA DE ENTRADA
+
+import android.app.ActivityOptions;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -10,64 +15,64 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-public class PrincipalActivity extends AppCompatActivity
-{
-    public static AlmacenDemandas almacen= new AlmacenDemandasArray();
+public class PrincipalActivity extends AppCompatActivity {
+    public static AlmacenDemandas almacen = new AlmacenDemandasArray();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
-        setSupportActionBar(toolbar);
-       // getSupportActionBar().setTitle("title vbar");
-      /*  toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(PrincipalActivity.this, "Atrás", Toast.LENGTH_SHORT).show();
-            }
-        });*/
+
+
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu1, menu);
         return super.onCreateOptionsMenu(menu);
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Intent i;
         switch (item.getItemId()) {
             case R.id.action_perfiles:
                 i = new Intent(this, ListadoPerfilesActivity.class);
-                startActivity(i);
+                startActivity(i,
+                        ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
                 return true;
             case R.id.crear_demandas:
 
                 i = new Intent(this, DemandaServicios.class);
-                startActivity(i);
+                startActivity(i,
+                        ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
                 return true;
 
             case R.id.action_demandas:
-                 i = new Intent(this, Demandas.class);
-                 startActivity(i);
+                i = new Intent(this, Demandas.class);
+                startActivity(i,
+                        ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
                 return true;
 
             case R.id.action_perfil:
-                 i = new Intent(this, Perfil.class);
-                 startActivity(i);
-
+                i = new Intent(this, Perfil.class);
+                startActivity(i,
+                        ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
                 return true;
-            case R.id.action_salir:
+
+                       case R.id.action_salir:
                 finish();
-                Intent intent = new Intent(Intent.ACTION_MAIN);
-                intent.addCategory(Intent.CATEGORY_HOME);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
+                i = new Intent(Intent.ACTION_MAIN);
+                i.addCategory(Intent.CATEGORY_HOME);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
                 return true;
 
             case R.id.action_settings:
-                 i = new Intent(this, RegistroDatosPersonales.class);
-                 startActivity(i);
+                i = new Intent(this, RegistroDatosPersonales.class);
+                startActivity(i,
+                        ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -79,18 +84,19 @@ public class PrincipalActivity extends AppCompatActivity
     }
     public void lanzarServicios(View view) {
         Intent i = new Intent(this, ListadoPerfilesActivity.class);
-        startActivity(i);
+        startActivity(i,
+                ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
     }
     public void lanzarDemandas(View view) {
         Intent i = new Intent(this, DemandaServicios.class);
-        startActivity(i);
+        startActivity(i,ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
     }
     public void lanzarPerfil(View view) {
         Intent i = new Intent(this, Perfil.class);
-        startActivity(i);
-        }
+        startActivity(i,ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+    }
     public void lanzarPuntuaciones(View view) {
         Intent i = new Intent(this, Demandas.class);
-        startActivity(i);
+        startActivity(i,ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
     }
 }//fin clase
