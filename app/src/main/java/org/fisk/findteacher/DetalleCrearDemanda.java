97349f1a06c241850de78e0458fcba165329eaff
Freddy Maila <fredosw@hotmail.com>
2017-12-14 22:13:54 -0500

    Monetización anuncio intersticial

--- app/src/main/java/org/fisk/findteacher/DetalleCrearDemanda.java
+++ app/src/main/java/org/fisk/findteacher/DetalleCrearDemanda.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -13,21 +14,36 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+
 /**
  * Created by Kelvin on 9/11/2017.
  */
 
 public class DetalleCrearDemanda extends AppCompatActivity {
+    private int x=1;
 
     ImageView imagenPrincipal;
     TextView profesion;
     TextView descripcion;
 
+    private InterstitialAd interstitialAd; //anuncio Intersticial
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_demanda);
+
+        //anuncio Intersticial
+        interstitialAd = new InterstitialAd(this);
+        interstitialAd.setAdUnitId("ca-app-pub-8547153403758959/9619838999");
+        interstitialAd.loadAd(new AdRequest.Builder().addTestDevice("80E5189E21CEB5DB740CF86FFE4941CC").build());
+
+
+
+        //fin anuncio Intersticial
 
         Bundle bundle = getIntent().getExtras();
         Usuario usuario = (Usuario) bundle.getSerializable("usuario");
@@ -41,6 +57,18 @@ public class DetalleCrearDemanda extends AppCompatActivity {
 
         profesion.setText(usuario.getProfesion());
         descripcion.setText(usuario.getDescripcion());
+
+        interstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                interstitialAd.loadAd(new AdRequest.Builder().addTestDevice("ca-app-pub-8547153403758959/9619838999").build());
+
+               // Log.d("anuncio","cerro anuncio" );
+
+                Intent i = new Intent(DetalleCrearDemanda.this, PrincipalActivity.class);
+                startActivity(i);
+            }
+        });
 
         //Log.d("User", usuario.getNombre());
     }
@@ -62,7 +90,7 @@ public class DetalleCrearDemanda extends AppCompatActivity {
             case R.id.mis_demandas:
                 i = new Intent(this, ListadoVerMisDemandas.class);
                 startActivity(i);
-              //  startActivity(i,  ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+                //  startActivity(i,  ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
                 return true;
 
 
@@ -80,11 +108,18 @@ public class DetalleCrearDemanda extends AppCompatActivity {
     }
     //=========================================================
 
-    public void demandaCreada(View v){
+    public void demandaCreada(View v) {
 
-        Toast.makeText(this, getResources().getString(R.string.demanda_creada), Toast.LENGTH_SHORT).show();
-        Intent i = new Intent(this, PrincipalActivity.class);
-        startActivity(i);
+        if (interstitialAd.isLoaded()) {
+            interstitialAd.show();
+            x=1;
+           // Log.d("anuncio","salio anuncio" );
+        } else {
+            Toast.makeText(DetalleCrearDemanda.this, "El Anuncio no esta disponible aun", Toast.LENGTH_LONG).show();
+        }
+
+      Toast.makeText(this, getResources().getString(R.string.demanda_creada), Toast.LENGTH_SHORT).show();
+
     }
 
 
